38a350d27341e00194b087221491d9d4b82b6a73
mrmamadi <mrmamadi@outlook.com>
2022-09-19 10:48:13 +0200

    remove aria-selected. remove commented out code. fix spacing.

--- js/nav.js
+++ js/nav.js
@@ -1,46 +1,46 @@
-const primaryNav = document.querySelector('.nav-bar');
-const navToggle = document.querySelector('.toggle');
+const primaryNav = document.querySelector(".nav-bar");
+const navToggle = document.querySelector(".toggle");
+// navToggle.style.backgroundImage = url("../img/icon_close.jpg");
 
-navToggle.addEventListener('click',() => {
-    const vis = primaryNav.getAttribute('data-visible')
-    if (vis === "false") {
-        primaryNav.setAttribute('data-visible','true');
-        navToggle.setAttribute('aria-selected','true')
-    } else {
-        primaryNav.setAttribute('data-visible','false')
-        navToggle.setAttribute('aria-selected','false')
-    }
+navToggle.addEventListener("click", () => {
+	const vis = primaryNav.getAttribute("data-visible");
+	if (vis === "false") {
+		primaryNav.setAttribute("data-visible", "true");
+		navToggle.style.backgroundImage = 'url("../img/icon_close.jpg")';
+	} else {
+		primaryNav.setAttribute("data-visible", "false");
+		navToggle.style.backgroundImage = 'url("../img/icon_menu.png")';
+	}
 });
-
 
 const slider = document.querySelector(".project-boxes");
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener("mousedown", e => {
-  slider.style.cursor = 'grabbing';
-  slider.style.userSelect = 'none';
-  isDown = true;
-  slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+slider.addEventListener("mousedown", (e) => {
+	slider.style.cursor = "grabbing";
+	slider.style.userSelect = "none";
+	isDown = true;
+	slider.classList.add("active");
+	startX = e.pageX - slider.offsetLeft;
+	scrollLeft = slider.scrollLeft;
 });
 
 slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
+	isDown = false;
+	slider.classList.remove("active");
 });
 
 slider.addEventListener("mouseup", () => {
-  isDown = false;
-  slider.classList.remove("active");
+	isDown = false;
+	slider.classList.remove("active");
 });
 
-slider.addEventListener("mousemove", e => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = x - startX;
-  slider.scrollLeft = scrollLeft - walk;
+slider.addEventListener("mousemove", (e) => {
+	if (!isDown) return;
+	e.preventDefault();
+	const x = e.pageX - slider.offsetLeft;
+	const walk = x - startX;
+	slider.scrollLeft = scrollLeft - walk;
 });
